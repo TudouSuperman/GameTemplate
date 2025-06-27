630cef6475784586d8632dc4360a59227f6dba8c
TudouSuperman <www.shiqi.com@gmail.com>
2025-06-27 14:30:59 +0800

    UP

--- Client/Assets/Scripts/GameApp/UI/UGuiExtension/ComponentEx/Runtime/ProgressBarCom.cs
+++ Client/Assets/Scripts/GameApp/UI/UGuiExtension/ComponentEx/Runtime/ProgressBarCom.cs
@@ -12,87 +12,39 @@ namespace GameApp.UI.Extension
         [SerializeField]
         private TextMeshProUGUI m_ProgressTextView;
 
-        public void RefreshView01(int currentValue, int totalValue)
-        {
-            if (totalValue <= 0 || currentValue < 0)
-            {
-                m_ProgressBarView.fillAmount = 0f;
-                m_ProgressTextView.text = string.Empty;
-                return;
-            }
-
-            float _progress = Mathf.Clamp01((float)currentValue / totalValue);
-            m_ProgressBarView.fillAmount = _progress;
-            m_ProgressTextView.text = $"{currentValue}/{totalValue}";
-        }
-
-        public void RefreshView01(float currentValue, float totalValue)
-        {
-            if (totalValue <= 0 || currentValue < 0)
-            {
-                m_ProgressBarView.fillAmount = 0f;
-                m_ProgressTextView.text = string.Empty;
-                return;
-            }
-
-            float _progress = Mathf.Clamp01(currentValue / totalValue);
-            m_ProgressBarView.fillAmount = _progress;
-            m_ProgressTextView.text = $"{(int)currentValue}/{(int)totalValue}";
-        }
-
-        public void RefreshView01Single(float currentValue, float totalValue)
-        {
-            if (totalValue <= 0 || currentValue < 0)
-            {
-                m_ProgressBarView.fillAmount = 0f;
-                m_ProgressTextView.text = string.Empty;
-                return;
-            }
-
-            float _progress = Mathf.Clamp01(currentValue / totalValue);
-            m_ProgressBarView.fillAmount = _progress;
-            m_ProgressTextView.text = $"{(int)currentValue}";
-        }
-
         public void RefreshView10(int currentValue, int totalValue)
         {
-            if (totalValue <= 0 || currentValue < 0)
-            {
-                m_ProgressBarView.fillAmount = 1f;
-                m_ProgressTextView.text = string.Empty;
-                return;
-            }
-
-            float _progress = 1f - Mathf.Clamp01((float)currentValue / totalValue);
-            m_ProgressBarView.fillAmount = _progress;
+            m_ProgressBarView.fillAmount = Mathf.Clamp01((float)currentValue / totalValue);
             m_ProgressTextView.text = $"{currentValue}/{totalValue}";
         }
 
         public void RefreshView10(float currentValue, float totalValue)
         {
-            if (totalValue <= 0 || currentValue < 0)
-            {
-                m_ProgressBarView.fillAmount = 1f;
-                m_ProgressTextView.text = string.Empty;
-                return;
-            }
+            m_ProgressBarView.fillAmount = Mathf.Clamp01(currentValue / totalValue);
+            m_ProgressTextView.text = $"{(int)currentValue}/{(int)totalValue}";
+        }
+        
+        public void RefreshView10Single(float currentValue, float totalValue)
+        {
+            m_ProgressBarView.fillAmount = Mathf.Clamp01(currentValue / totalValue);
+            m_ProgressTextView.text = $"{(int)currentValue}";
+        }
 
-            float _progress = 1f - Mathf.Clamp01(currentValue / totalValue);
-            m_ProgressBarView.fillAmount = _progress;
+        public void RefreshView01(int currentValue, int totalValue)
+        {
+            m_ProgressBarView.fillAmount = 1f - Mathf.Clamp01((float)currentValue / totalValue);
+            m_ProgressTextView.text = $"{currentValue}/{totalValue}";
+        }
+
+        public void RefreshView01(float currentValue, float totalValue)
+        {
+            m_ProgressBarView.fillAmount = 1f - Mathf.Clamp01(currentValue / totalValue);
             m_ProgressTextView.text = $"{(int)currentValue}/{(int)totalValue}";
         }
 
-        public void RefreshView10Single(float currentValue, float totalValue)
+        public void RefreshView01Single(float currentValue, float totalValue)
         {
-            if (totalValue <= 0 || currentValue < 0)
-            {
-                m_ProgressBarView.fillAmount = 1f;
-                m_ProgressTextView.text = string.Empty;
-                return;
-            }
-
-            float _progress = 1f - Mathf.Clamp01(currentValue / totalValue);
-            m_ProgressBarView.fillAmount = _progress;
+            m_ProgressBarView.fillAmount = 1f - Mathf.Clamp01(currentValue / totalValue);
             m_ProgressTextView.text = $"{(int)currentValue}";
         }
     }
