e7ce644d4df773aa0f8e6bb4755bbd6793075706
TudouSuperman <www.shiqi.com@gmail.com>
2025-07-02 19:27:29 +0800

    UP

---
+++ Client/Assets/Scripts/GameApp/UI/UGuiExtension/Graphic/Runtime/RadarGraphic.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameApp.UI.Extension
+{
+    /// <summary>
+    /// 在 UGUI 中绘制雷达图组件。
+    /// </summary>
+    [RequireComponent(typeof(CanvasRenderer))]
+    public sealed class RadarGraphic : MaskableGraphic
+    {
+        [Range(0f, 300f)]
+        [SerializeField]
+        private float m_Radius = 100f;
+
+        [Tooltip("每个方向的百分比（0-1）")]
+        [SerializeField]
+        private float[] m_Percents;
+
+        public void Refresh()
+        {
+            SetVerticesDirty();
+        }
+
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
+            if (null == m_Percents || m_Percents.Length < 3) return;
+            int _count = m_Percents.Length;
+            // 添加中心点。
+            UIVertex _center = UIVertex.simpleVert;
+            _center.color = color;
+            _center.position = Vector3.zero;
+            vh.AddVert(_center);
+            // 添加外围点。
+            for (int i = 0; i < _count; i++)
+            {
+                // 顺时针。
+                float _angle = Mathf.PI / 2 - i * Mathf.PI * 2 / _count;
+                float _percent = Mathf.Clamp01(m_Percents[i]);
+                float _x = Mathf.Cos(_angle) * m_Radius * _percent;
+                float _y = Mathf.Sin(_angle) * m_Radius * _percent;
+                UIVertex _vert = UIVertex.simpleVert;
+                _vert.color = color;
+                _vert.position = new Vector3(_x, _y, 0);
+                vh.AddVert(_vert);
+            }
+            // 三角形绘制。
+            for (int i = 1; i <= _count; i++)
+            {
+                int _next = i == _count ? 1 : i + 1;
+                vh.AddTriangle(0, i, _next);
+            }
+        }
+
+#if UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            Refresh();
+        }
+#endif
+    }
+}
