37e0bec7825afc81dbb2e99ab60c78fcf3e25414
TudouSuperman <www.shiqi.com@gmail.com>
2025-07-24 11:11:56 +0800

    UP

---
+++ Client/Assets/Scripts/GameApp/Editor/Build/BuildHelper.cs
@@ -0,0 +1,148 @@
+﻿using System.IO;
+using GameFramework;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityGameFramework.Editor.ResourceTools;
+using UnityGameFramework.Extension.Editor;
+
+namespace GameApp.Editor
+{
+    public static class BuildHelper
+    {
+        public static readonly string BuildPkgFolder = Path.GetFullPath("../ClientBuild/Build_Package");
+
+        // [InitializeOnLoadMethod]
+        public static void ReGenerateProjectFiles()
+        {
+            Unity.CodeEditor.CodeEditor.CurrentEditor.SyncAll();
+            Debug.Log("ReGenerateProjectFiles finished.");
+        }
+
+        public static BuildReport BuildPkg(Platform platform)
+        {
+            BuildTarget buildTarget = BuildTarget.NoTarget;
+            string appName = Application.productName;
+            switch (platform)
+            {
+                case Platform.Windows:
+                    buildTarget = BuildTarget.StandaloneWindows;
+                    appName += ".exe";
+                    break;
+                case Platform.Windows64:
+                    buildTarget = BuildTarget.StandaloneWindows64;
+                    appName += ".exe";
+                    break;
+                case Platform.Android:
+                    buildTarget = BuildTarget.Android;
+                    appName += ".apk";
+                    break;
+                case Platform.IOS:
+                    buildTarget = BuildTarget.iOS;
+                    break;
+                case Platform.MacOS:
+                    buildTarget = BuildTarget.StandaloneOSX;
+                    break;
+                case Platform.Linux:
+                    buildTarget = BuildTarget.StandaloneLinux64;
+                    break;
+                case Platform.WebGL:
+                    buildTarget = BuildTarget.WebGL;
+                    break;
+                default:
+                    throw new GameFrameworkException($"No Support {platform}!");
+            }
+
+            Debug.Log($"start build {platform}");
+            
+            string fold = Utility.Path.GetRegularPath($"{BuildPkgFolder}/{platform}");
+
+            if (Directory.Exists(fold))
+            {
+                FileTool.CleanDirectory(fold);
+            }
+            else
+            {
+                Directory.CreateDirectory(fold);
+            }
+            
+            BuildResource(platform);
+
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+            
+            string[] levels =
+            {
+                EntryUtility.EntryScenePath,
+            };
+            Debug.Log("start build Package");
+            string locationPathName = $"{fold}/{appName}";
+#if !UNITY_HOTFIX && UNITY_ET && UNITY_2021//兼容ET的Bson
+            EditorUserBuildSettings.il2CppCodeGeneration = Il2CppCodeGeneration.OptimizeSize;
+#elif UNITY_2021
+            EditorUserBuildSettings.il2CppCodeGeneration = Il2CppCodeGeneration.OptimizeSpeed;
+#endif
+#if !UNITY_HOTFIX && UNITY_ET && UNITY_2022_1_OR_NEWER//兼容ET的Bson
+            PlayerSettings.SetIl2CppCodeGeneration(NamedBuildTarget.FromBuildTargetGroup(BuildPipeline.GetBuildTargetGroup(buildTarget)), Il2CppCodeGeneration.OptimizeSize);
+#elif UNITY_2022_1_OR_NEWER
+            PlayerSettings.SetIl2CppCodeGeneration(NamedBuildTarget.FromBuildTargetGroup(BuildPipeline.GetBuildTargetGroup(buildTarget)), Il2CppCodeGeneration.OptimizeSpeed);
+#endif
+            BuildReport buildReport = BuildPipeline.BuildPlayer(levels, locationPathName, buildTarget, BuildOptions.None);
+            if (buildReport.summary.result != BuildResult.Succeeded)
+            {
+                throw new GameFrameworkException($"build Package fail : { buildReport.summary.result }");
+            }
+            Debug.Log($"finish build Package at {locationPathName}");
+            if (!Application.isBatchMode)
+            {
+                EditorUtility.OpenWithDefaultApp(fold);
+            }
+            return buildReport;
+        }
+
+        public static void BuildResource(Platform platform)
+        {
+            Debug.Log("start refresh resource collection");
+            ResourceRuleEditorUtility.RefreshResourceCollection();
+            Debug.Log("finish refresh resource collection");
+            
+            Debug.Log("start build resource");
+            ResourceBuildHelper.StartBuild(platform);
+            Debug.Log("finish build resource");
+        }
+
+        public static void RefreshWindowsPkgResource()
+        {
+            string targetPath = $"{BuildPkgFolder}/{Platform.Windows}/{Application.productName}_Data/StreamingAssets/";
+            if (!Directory.Exists(targetPath))
+            {
+                throw new GameFrameworkException($"RefreshExePkgResource fail! {targetPath} not exist!");
+            }
+            
+            BuildResource(Platform.Windows);
+            
+            FileTool.CleanDirectory(targetPath);
+            string bundleFold = Path.Combine(ResourceBuildHelper.GetNewestBundlePath(), Platform.Windows.ToString());
+            bundleFold = Utility.Path.GetRegularPath(bundleFold);
+            FileTool.CopyDirectory(bundleFold, targetPath);
+            Debug.Log($"src dir: {bundleFold}    target: {targetPath}");
+        }
+        
+        public static void RefreshWindows64PkgResource()
+        {
+            string targetPath = $"{BuildPkgFolder}/{Platform.Windows64}/{Application.productName}_Data/StreamingAssets/";
+            if (!Directory.Exists(targetPath))
+            {
+                throw new GameFrameworkException($"RefreshExePkgResource fail! {targetPath} not exist!");
+            }
+            
+            BuildResource(Platform.Windows64);
+            
+            FileTool.CleanDirectory(targetPath);
+            string bundleFold = Path.Combine(ResourceBuildHelper.GetNewestBundlePath(), Platform.Windows64.ToString());
+            bundleFold = Utility.Path.GetRegularPath(bundleFold);
+            FileTool.CopyDirectory(bundleFold, targetPath);
+            Debug.Log($"src dir: {bundleFold}    target: {targetPath}");
+        }
+    }
+}
---
+++ Client/Assets/Scripts/GameApp/Editor/Build/BuildToolEditor.cs
@@ -0,0 +1,144 @@
+using System.IO;
+using GameFramework;
+using UnityEditor;
+using UnityEngine;
+using UnityGameFramework.Editor;
+using UnityGameFramework.Editor.ResourceTools;
+
+namespace GameApp.Editor
+{
+    public class BuildToolEditor : EditorWindow
+    {
+        private Platform m_Platform;
+
+        [MenuItem("GameApp/Build Tool Editor")]
+        public static void ShowWindow()
+        {
+            GetWindow<BuildToolEditor>("Build Tool");
+        }
+
+        public Platform GetCurPlatform()
+        {
+#if UNITY_IOS
+            return Platform.IOS;
+#elif UNITY_ANDROID
+            return Platform.Android;
+#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            return Platform.Windows64;
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+            return Platform.MacOS;
+#endif
+        }
+
+        private void OnEnable()
+        {
+            m_Platform = GetCurPlatform();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUI.BeginDisabledGroup(Application.isPlaying);
+            {
+                m_Platform = (Platform)EditorGUILayout.EnumPopup("Platform: ", m_Platform);
+
+                if (GUILayout.Button("Build Package (Contains Rebuild Resource)"))
+                {
+                    if (SwitchActiveBuildTarget(m_Platform))
+                    {
+                        BuildHelper.BuildPkg(m_Platform);
+                        Debug.Log($"Build {m_Platform} Package Success!");
+                    }
+                }
+
+                if (GUILayout.Button("Build Resource"))
+                {
+                    if (SwitchActiveBuildTarget(m_Platform))
+                    {
+                        BuildHelper.BuildResource(m_Platform);
+                        Debug.Log($"Build {m_Platform} Resource Success!");
+                    }
+                }
+
+                GUILayout.Space(20);
+                if (GUILayout.Button("Build And Refresh Windows64 Package Resource"))
+                {
+                    if (SwitchActiveBuildTarget(Platform.Windows64))
+                    {
+                        BuildHelper.RefreshWindows64PkgResource();
+                        Debug.Log("Build Model Success!");
+                    }
+                }
+
+                if (GUILayout.Button("Open Package Folder"))
+                {
+                    string folderPath = $"{BuildHelper.BuildPkgFolder}/{m_Platform}";
+                    if (Directory.Exists(folderPath))
+                    {
+                        OpenFolder.Execute(folderPath);
+                    }
+                    else
+                    {
+                        Debug.LogError($"Open folder fail! {folderPath} not exist!");
+                    }
+                }
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+
+        private bool SwitchActiveBuildTarget(Platform platform)
+        {
+            if (platform != GetCurPlatform())
+            {
+                switch (EditorUtility.DisplayDialogComplex("Warning!",
+                            $"current platform is {GetCurPlatform()}, if change to {platform}, may be take a long time",
+                            "change", "cancel", null))
+                {
+                    case 0:
+                        break;
+                    default:
+                        return false;
+                }
+            }
+
+            BuildTarget buildTarget = BuildTarget.NoTarget;
+            BuildTargetGroup buildTargetGroup = BuildTargetGroup.Unknown;
+            switch (platform)
+            {
+                case Platform.Windows:
+                    buildTarget = BuildTarget.StandaloneWindows;
+                    buildTargetGroup = BuildTargetGroup.Standalone;
+                    break;
+                case Platform.Windows64:
+                    buildTarget = BuildTarget.StandaloneWindows64;
+                    buildTargetGroup = BuildTargetGroup.Standalone;
+                    break;
+                case Platform.Android:
+                    buildTarget = BuildTarget.Android;
+                    buildTargetGroup = BuildTargetGroup.Android;
+                    break;
+                case Platform.IOS:
+                    buildTarget = BuildTarget.iOS;
+                    buildTargetGroup = BuildTargetGroup.iOS;
+                    break;
+                case Platform.MacOS:
+                    buildTarget = BuildTarget.StandaloneOSX;
+                    buildTargetGroup = BuildTargetGroup.Standalone;
+                    break;
+                case Platform.Linux:
+                    buildTarget = BuildTarget.StandaloneLinux64;
+                    buildTargetGroup = BuildTargetGroup.Standalone;
+                    break;
+                case Platform.WebGL:
+                    buildTarget = BuildTarget.WebGL;
+                    buildTargetGroup = BuildTargetGroup.WebGL;
+                    break;
+                default:
+                    throw new GameFrameworkException($"No Support {platform}!");
+            }
+
+            EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+            return true;
+        }
+    }
+}
