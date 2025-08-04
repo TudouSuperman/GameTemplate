0b710be787cdaaa7a995f12be15fd38ff075cb50
TudouSuperman <www.shiqi.com@gmail.com>
2025-08-04 11:34:16 +0800

    UP

--- Client/Assets/Scripts/GameApp/HotUpdate/Code/Runtime/UI/Setting/SettingFormLogic.cs
+++ Client/Assets/Scripts/GameApp/HotUpdate/Code/Runtime/UI/Setting/SettingFormLogic.cs
@@ -7,7 +7,7 @@ namespace GameApp.Hot.UI
         protected override void OnInit(object userData)
         {
             base.OnInit(userData);
-            
+
             SettingFormView _view = (SettingFormView)m_UGuiFormView;
             _view.OnMusicMuteChanged += OnMusicMuteChanged;
             _view.OnMusicVolumeChanged += OnMusicVolumeChanged;
@@ -25,21 +25,21 @@ namespace GameApp.Hot.UI
         protected override void OnOpen(object userData)
         {
             base.OnOpen(userData);
-            
+
             SettingFormView _view = (SettingFormView)m_UGuiFormView;
-            _view.SetMusic(!GameEntry.Sound.IsMuted("Music"));
-            _view.SetMusicVolume(GameEntry.Sound.GetVolume("Music"));
-            _view.SetSound(!GameEntry.Sound.IsMuted("Sound"));
-            _view.SetSoundVolume(GameEntry.Sound.GetVolume("Sound"));
-            _view.SetUISound(!GameEntry.Sound.IsMuted("UISound"));
-            _view.SetUISoundVolume(GameEntry.Sound.GetVolume("UISound"));
+            _view.SetMusic(!GameEntry.Sound.IsMuted(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.Music))));
+            _view.SetMusicVolume(GameEntry.Sound.GetVolume(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.Music))));
+            _view.SetSound(!GameEntry.Sound.IsMuted(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.Sound))));
+            _view.SetSoundVolume(GameEntry.Sound.GetVolume(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.Sound))));
+            _view.SetUISound(!GameEntry.Sound.IsMuted(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.UISound))));
+            _view.SetUISoundVolume(GameEntry.Sound.GetVolume(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.UISound))));
         }
 
-        private void OnMusicMuteChanged(bool isOn) => GameEntry.Sound.Mute("Music", !isOn);
-        private void OnMusicVolumeChanged(float volume) => GameEntry.Sound.SetVolume("Music", volume);
-        private void OnSoundMuteChanged(bool isOn) => GameEntry.Sound.Mute("Sound", !isOn);
-        private void OnSoundVolumeChanged(float volume) => GameEntry.Sound.SetVolume("Sound", volume);
-        private void OnUISoundMuteChanged(bool isOn) => GameEntry.Sound.Mute("UISound", !isOn);
-        private void OnUISoundVolumeChanged(float volume) => GameEntry.Sound.SetVolume("UISound", volume);
+        private void OnMusicMuteChanged(bool isOn) => GameEntry.Sound.Mute(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.Music)), !isOn);
+        private void OnMusicVolumeChanged(float volume) => GameEntry.Sound.SetVolume(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.Music)), volume);
+        private void OnSoundMuteChanged(bool isOn) => GameEntry.Sound.Mute(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.Sound)), !isOn);
+        private void OnSoundVolumeChanged(float volume) => GameEntry.Sound.SetVolume(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.Sound)), volume);
+        private void OnUISoundMuteChanged(bool isOn) => GameEntry.Sound.Mute(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.UISound)), !isOn);
+        private void OnUISoundVolumeChanged(float volume) => GameEntry.Sound.SetVolume(GameFramework.Utility.Text.Format("{0}", nameof(ESoundGroupID.UISound)), volume);
     }
 }
